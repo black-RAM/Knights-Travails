8090452d1f61455dafef9b58ba43f861e104172c
black-RAM <pplanes311@gmail.com>
2024-02-16 12:20:13 +0300

    possible knight moves are within the bounds of the board

--- index.js
+++ index.js
@@ -11,8 +11,29 @@ function knightMoves(start) {
   const wsw = [x - 2, y - 1]
   const wnw = [x - 2, y + 1]
   const nnw = [x - 1, y + 2]
-  return [nne, ene, ese, sse, ssw, wsw, wnw, nnw]
+
+  // get coordinates that fit within bounds of board
+  const valid = [nne, ene, ese, sse, ssw, wsw, wnw, nnw]
+
+  for (let i = 0; i < valid.length; i++) {
+    const pos = valid[i]
+
+    const lowX = pos[0] < 0
+    const lowY = pos[1] < 0
+
+    const highX = pos[0] > 7
+    const highY = pos[1] > 7
+
+    const outside = lowX || lowY || highX || highY
+    
+    if(outside) {
+      valid.splice(i, 1)
+      i-- // deleting an element shifts indexes
+    }
+  }
+
+  return valid
 }
 
-const moves = knightMoves([3, 3])
+const moves = knightMoves([0, 0])
 console.log(moves)
